f5e0ab294b29867ffe030561258c3a5a0184a713
TQA <trinhquocanh94@gmail.com>
2019-01-23 11:18:30 +0100

    Solve raindrops and download new exercises.

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,20 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      String s = "";
+      if (number % 3 == 0){
+        s = s + "Pling";
+      }
+      if (number % 5 == 0){
+        s = s + "Plang";
+      }
+      if (number % 7 == 0){
+        s = s + "Plong";
+      }
+      if (s == ""){
+        return String.valueOf(number);
+      }
+      return s;
     }
 
 }
