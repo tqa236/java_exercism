e133aa906133ba6ace4e06c7b2c940da5f077a39
TQA <trinhquocanh94@gmail.com>
2019-01-23 19:54:42 +0100

    Solve hamming exercise.

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,11 +1,24 @@
 class Hamming {
+    String leftStrand;
+    String rightStrand;
+    int distance = 0;
 
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (leftStrand.length() != rightStrand.length()){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        for (int i = 0; i < this.leftStrand.length(); i++){
+          if (this.leftStrand.charAt(i) != this.rightStrand.charAt(i)){
+            this.distance = this.distance + 1;
+          }
+        }
+        return this.distance;
+
     }
 
 }
