3cde636f9d62b0b3c0f73c2f1b22b00448cafe53
TQA <trinhquocanh94@gmail.com>
2019-01-24 22:55:39 +0100

    Update Hamming and download new exercises.

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -2,14 +2,14 @@ class Hamming {
   private String leftStrand;
   private String rightStrand;
 
-  public Hamming(String leftStrand, String rightStrand) {
+  Hamming(String leftStrand, String rightStrand) {
     if (leftStrand.length() != rightStrand.length()) {
       throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
     }
     this.leftStrand = leftStrand;
     this.rightStrand = rightStrand;
   }
-  
+
   int getHammingDistance() {
     int distance = 0;
     for (int i = 0; i < leftStrand.length(); i++) {
