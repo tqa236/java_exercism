8e63f10f493cab00c2041aa8c096684419e95801
TQA <trinhquocanh94@gmail.com>
2019-02-07 14:01:36 +0100

    Solve some exercises

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,16 @@
+import java.lang.Math;
+
+
 class ArmstrongNumbers {
-
 	boolean isArmstrongNumber(int numberToCheck) {
-
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-		
+		int number = numberToCheck;
+		int armstrongNumbers = 0;
+		int numDigit = (int)Math.ceil(Math.log10(number));
+		while (number > 0){
+			armstrongNumbers += (int)Math.pow(number % 10, numDigit);
+			number = number/ 10;
+		}
+		return armstrongNumbers == numberToCheck;
 	}
 
 }
--- armstrong-numbers/src/test/java/ArmstrongNumbersTest.java
+++ armstrong-numbers/src/test/java/ArmstrongNumbersTest.java
@@ -17,11 +17,10 @@ public class ArmstrongNumbersTest {
     @Test
     public void singleDigitsAreArmstrongNumbers() {
         int input = 5;
-
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void noTwoDigitArmstrongNumbers() {
         int input = 10;
@@ -29,51 +28,51 @@ public class ArmstrongNumbersTest {
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void threeDigitNumberIsArmstrongNumber() {
         int input = 153;
-        
+
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void threeDigitNumberIsNotArmstrongNumber() {
         int input = 100;
-        
+
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void fourDigitNumberIsArmstrongNumber() {
         int input = 9474;
-        
+
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void fourDigitNumberIsNotArmstrongNumber() {
         int input = 9475;
-        
+
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void sevenDigitNumberIsArmstrongNumber() {
         int input = 9926315;
-        
+
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void sevenDigitNumberIsNotArmstrongNumber() {
         int input = 9926314;
-        
+
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
