6df6f3c17358f5d4cb53c0ec539e1539e1270d21
TQA <trinhquocanh94@gmail.com>
2019-03-01 21:33:29 +0100

    Solve some exercises

---
+++ matrix/src/main/java/Matrix.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Matrix {
+
+  private List<List<Integer>> rows;
+
+  Matrix(String matrixAsString) {
+    String[] lines = matrixAsString.split("\n");
+    rows =
+        Arrays.stream(lines)
+            .map(
+                line ->
+                    Arrays.stream(line.split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList()))
+            .collect(Collectors.toList());
+  }
+
+  int[] getRow(int rowNumber) {
+    return rows.get(rowNumber).stream().mapToInt(i -> i).toArray();
+  }
+
+  int[] getColumn(int columnNumber) {
+    return rows.stream().map(row -> row.get(columnNumber)).mapToInt(i -> i).toArray();
+  }
+}
--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -4,31 +4,26 @@ import java.util.HashMap;
 import java.util.List;
 
 class HandshakeCalculator {
-  private static final HashMap<Integer, Signal> values = new HashMap<Integer, Signal>();
+  private static final HashMap<Integer, Signal> actions = new HashMap<Integer, Signal>();
 
   static {
-    values.put(4, Signal.WINK);
-    values.put(3, Signal.DOUBLE_BLINK);
-    values.put(2, Signal.CLOSE_YOUR_EYES);
-    values.put(1, Signal.JUMP);
+    actions.put(1, Signal.WINK);
+    actions.put(2, Signal.DOUBLE_BLINK);
+    actions.put(4, Signal.CLOSE_YOUR_EYES);
+    actions.put(8, Signal.JUMP);
   }
 
   public List<Signal> calculateHandshake(int number) {
     List<Signal> signal = new ArrayList<Signal>();
-    String binary = Integer.toBinaryString(number % 32);
-    int missing_length = 5 - binary.length();
 
-    for (int i = 0; i < missing_length; i = i + 1) {
-      binary = "0" + binary;
-    }
+    actions.forEach(
+        (point, action) -> {
+          if ((number & point) == point) {
+            signal.add(action);
+          }
+        });
 
-    for (Integer i = 4; i > 0; i = i - 1) {
-      if (binary.charAt(i) == '1') {
-        signal.add(values.get(i));
-      }
-    }
-
-    if (binary.charAt(0) == '1') {
+    if ((number & 16) == 16) {
       Collections.reverse(signal);
     }
 
