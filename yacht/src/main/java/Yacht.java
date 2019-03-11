337f04b425361e043cd85fcc85712a43b168cc8d
TQA <trinhquocanh94@gmail.com>
2019-03-11 19:05:04 +0100

    Solve Yatch

--- yacht/src/main/java/Yacht.java
+++ yacht/src/main/java/Yacht.java
@@ -1,12 +1,61 @@
+import java.util.Arrays;
+
 class Yacht {
+  private int score;
 
   Yacht(int[] dice, YachtCategory yachtCategory) {
-    throw new UnsupportedOperationException(
-        "Delete this statement and write your own implementation.");
+    // IntStream dices = Arrays.stream(dice);
+    int value = yachtCategory.getValue();
+    long distinctValue = Arrays.stream(dice).distinct().count();
+    int sumDice = Arrays.stream(dice).sum();
+    switch (yachtCategory) {
+      case YACHT:
+        if (distinctValue == 1) {
+          score = value;
+        }
+        break;
+      case ONES:
+      case TWOS:
+      case THREES:
+      case FOURS:
+      case FIVES:
+      case SIXES:
+        score = value * (int) Arrays.stream(dice).filter(num -> num == value).count();
+        break;
+      case FULL_HOUSE:
+        if (distinctValue == 2) {
+          switch (Math.toIntExact(Arrays.stream(dice).filter(num -> num == dice[0]).count())) {
+            case 2:
+            case 3:
+              score = sumDice;
+          }
+        }
+        break;
+      case FOUR_OF_A_KIND:
+        if (distinctValue <= 2) {
+          switch (Math.toIntExact(Arrays.stream(dice).filter(num -> num == dice[0]).count())) {
+            case 1:
+              score = sumDice - dice[0];
+              break;
+            case 4:
+            case 5:
+              score = dice[0] * 4;
+          }
+        }
+        break;
+      case LITTLE_STRAIGHT:
+      case BIG_STRAIGHT:
+        if (distinctValue == 5 && sumDice == value) {
+          score = 30;
+        }
+        break;
+      case CHOICE:
+        score = sumDice;
+        break;
+    }
   }
 
   public int score() {
-    throw new UnsupportedOperationException(
-        "Delete this statement and write your own implementation.");
+    return score;
   }
 }
--- yacht/src/main/java/YachtCategory.java
+++ yacht/src/main/java/YachtCategory.java
@@ -1,14 +1,24 @@
 enum YachtCategory {
-  YACHT,
-  ONES,
-  TWOS,
-  THREES,
-  FOURS,
-  FIVES,
-  SIXES,
-  FULL_HOUSE,
-  FOUR_OF_A_KIND,
-  LITTLE_STRAIGHT,
-  BIG_STRAIGHT,
-  CHOICE
+  YACHT(50),
+  ONES(1),
+  TWOS(2),
+  THREES(3),
+  FOURS(4),
+  FIVES(5),
+  SIXES(6),
+  FULL_HOUSE(7),
+  FOUR_OF_A_KIND(8),
+  LITTLE_STRAIGHT(15),
+  BIG_STRAIGHT(20),
+  CHOICE(11);
+
+  private int value;
+
+  YachtCategory(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
 }
