a741b167fbc2d676c70c61a57624550ed4420f7b
TQA <trinhquocanh94@gmail.com>
2019-03-11 22:15:58 +0100

    Solve some exercises

--- proverb/src/main/java/Proverb.java
+++ proverb/src/main/java/Proverb.java
@@ -1,12 +1,19 @@
 class Proverb {
 
+  private String[] objects;
+
   Proverb(String[] words) {
-    throw new UnsupportedOperationException(
-        "Delete this statement and write your own implementation.");
+    objects = words;
   }
 
   public String recite() {
-    throw new UnsupportedOperationException(
-        "Delete this statement and write your own implementation.");
+    String proverb = "";
+    for (int i = 0; i < objects.length - 1; i++) {
+      proverb = proverb + "For want of a " + objects[i] + " the " + objects[i + 1] + " was lost.\n";
+    }
+    if (objects.length > 0) {
+      proverb = proverb + "And all for the want of a " + objects[0] + ".";
+    }
+    return proverb;
   }
 }
--- sieve/src/main/java/Sieve.java
+++ sieve/src/main/java/Sieve.java
@@ -1,13 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Sieve {
+
+  private int limit;
+
   Sieve(int maxPrime) {
-    throw new UnsupportedOperationException(
-        "Delete this statement and provide your own implementation.");
+    limit = maxPrime;
   }
 
   public List<Integer> getPrimes() {
-    throw new UnsupportedOperationException(
-        "Delete this statement and provide your own implementation.");
+    List<Integer> primes = new ArrayList<Integer>();
+    List<Integer> range = IntStream.rangeClosed(2, limit).boxed().collect(Collectors.toList());
+    while (!range.isEmpty()) {
+      Integer prime = range.get(0);
+      primes.add(prime);
+      range = range.stream().filter(p -> p % prime != 0).collect(Collectors.toList());
+    }
+
+    return primes;
   }
 }
