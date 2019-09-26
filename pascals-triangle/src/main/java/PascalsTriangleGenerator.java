09fe697b25bdaceb3fc4c74c9f7d71751b55d033
TQA <trinhquocanh94@gmail.com>
2019-09-26 23:37:00 +0200

    Solve pascals triangle

--- pascals-triangle/src/main/java/PascalsTriangleGenerator.java
+++ pascals-triangle/src/main/java/PascalsTriangleGenerator.java
@@ -1,9 +1,21 @@
+// import java.util.Arrays;
+
 class PascalsTriangleGenerator {
   public int[][] generateTriangle(int size) {
-    int[][] pascalsTriangle = new int[][] {};
+    int[][] pascalsTriangle = new int[size][];
+
     for (int i = 0; i < size; i++) {
-      int[] newLine = pascalsTriangle[i];
+      int[] newLine = new int[i + 1];
+      for (int j = 0; j < i + 1; j++) {
+        if ((j == 0) || (j == i)) {
+          newLine[j] = 1;
+        } else {
+          newLine[j] = pascalsTriangle[i - 1][j - 1] + pascalsTriangle[i - 1][j];
+        }
+      }
+      pascalsTriangle[i] = newLine;
     }
+    // System.out.println(Arrays.deepToString(pascalsTriangle));
     return pascalsTriangle;
   }
 }
