1a04d7adaa407478d788e4c33e4960cf1c7aaec1
TQA <trinhquocanh94@gmail.com>
2019-09-26 22:46:22 +0200

    Add new exercises

---
+++ pascals-triangle/src/main/java/PascalsTriangleGenerator.java
@@ -0,0 +1,9 @@
+class PascalsTriangleGenerator {
+  public int[][] generateTriangle(int size) {
+    int[][] pascalsTriangle = new int[][] {};
+    for (int i = 0; i < size; i++) {
+      int[] newLine = pascalsTriangle[i];
+    }
+    return pascalsTriangle;
+  }
+}
