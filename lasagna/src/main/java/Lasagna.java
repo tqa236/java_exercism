e138f87820b40d28fc48adf29fbfa97edd8f64ea
Anh Trinh <trinhquocanh94@gmail.com>
2024-01-01 11:30:56 +0100

    solve lasagna

---
+++ lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,17 @@
+public class Lasagna {
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    public int remainingMinutesInOven(int active_time) {
+        return 40 - active_time;
+    }
+
+    public int preparationTimeInMinutes(int layer) {
+        return 2 * layer;
+    }
+
+    public int totalTimeInMinutes(int layer, int active_time) {
+        return preparationTimeInMinutes(layer) + active_time;
+    }
+}
