05855051fe2c3418120b25c76833cd516860168c
Anh Trinh <trinhquocanh94@gmail.com>
2024-01-02 18:17:37 +0100

    solve bird watcher

---
+++ bird-watcher/src/main/java/BirdWatcher.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        return this.birdsPerDay;
+    }
+
+    public int getToday() {
+        return this.birdsPerDay[this.birdsPerDay.length - 1];
+    }
+
+    public void incrementTodaysCount() {
+        this.birdsPerDay[this.birdsPerDay.length - 1] += 1;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        return Arrays.stream(birdsPerDay).filter(a -> a == 0).findAny().isPresent();
+
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        return Arrays.stream(birdsPerDay).limit(numberOfDays).sum();
+    }
+
+    public int getBusyDays() {
+        return (int) Arrays.stream(birdsPerDay).filter(a -> a >= 5).count();
+    }
+}
