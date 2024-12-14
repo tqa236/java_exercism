be099d5a70b1c7ff719f95af11a07d8194c026ae
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2024-12-14 22:27:41 +0100

    Update exercise

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -3,6 +3,13 @@ import java.util.stream.Collectors;
 
 class SqueakyClean {
     static String clean(String identifier) {
+        identifier = identifier
+                .replace("4", "a")
+                .replace("3", "e")
+                .replace("0", "o")
+                .replace("1", "l")
+                .replace("7", "t");
+
         return Pattern.compile("-(.)").matcher(identifier.replaceAll(" ", "_").replaceAll("\\p{Cc}", "CTRL"))
                 .replaceAll(mr -> mr.group(1).toUpperCase()).chars()
                 .filter(c -> Character.isLetter(c) || c == '_')
