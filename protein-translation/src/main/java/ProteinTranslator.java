c525633e1d881be504089ea3403cd2f7b2570ba4
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2024-12-15 12:01:01 +0100

    Solve protein translation

--- protein-translation/src/main/java/ProteinTranslator.java
+++ protein-translation/src/main/java/ProteinTranslator.java
@@ -1,32 +1,48 @@
 import java.util.*;
 
 class ProteinTranslator {
-    private static final HashMap<String, String> values =
-                                new HashMap<String, String>(){{
+    private static final HashMap<String, String> values = new HashMap<String, String>() {{
         put("AUG", "Methionine");
-        put("UUU", "Phenylalanine");put("UUC", "Phenylalanine");
-        put("UUA", "Leucine");put("UUG", "Leucine");
-        put("UCU", "Serine");put("UCC", "Serine");
-        put("UCA", "Serine");put("UCG", "Serine");
-        put("UAU", "Tyrosine");put("UAC", "Tyrosine");
-        put("UGU", "Cysteine");put("UGC", "Cysteine");
+        put("UUU", "Phenylalanine");
+        put("UUC", "Phenylalanine");
+        put("UUA", "Leucine");
+        put("UUG", "Leucine");
+        put("UCU", "Serine");
+        put("UCC", "Serine");
+        put("UCA", "Serine");
+        put("UCG", "Serine");
+        put("UAU", "Tyrosine");
+        put("UAC", "Tyrosine");
+        put("UGU", "Cysteine");
+        put("UGC", "Cysteine");
         put("UGG", "Tryptophan");
-        put("UAA", "STOP");put("UAG", "STOP");put("UGA", "STOP");
+        put("UAA", "STOP");
+        put("UAG", "STOP");
+        put("UGA", "STOP");
     }};
-    String protein;
+    
     List<String> translate(String rnaSequence) {
         List<String> proteins = new ArrayList<>();
-        for(int i = 0; i < rnaSequence.length(); i = i + 3)
-        {
-            protein = values.get(rnaSequence.substring(i,i+3));
-            if (protein != "STOP"){
-                proteins.add(protein);
-            }
-            else{
-                break;
-            }
+        
+        for (int i = 0; i < rnaSequence.length(); i = i + 3) {
+            if (i + 3 <= rnaSequence.length()) {
+                String codon = rnaSequence.substring(i, i + 3);
+                String protein = values.get(codon);
 
+                if (protein == null) {
+                    throw new IllegalArgumentException("Invalid codon");
+                }
+
+                if (protein.equals("STOP")) {
+                    break;
+                }
+
+                proteins.add(protein);
+            } else {
+                throw new IllegalArgumentException("Invalid codon");
+            }
         }
+
         return proteins;
     }
 }
