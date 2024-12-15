2bd964314850ca3fe58ce705e2be799a9a39f031
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2024-12-15 13:07:00 +0100

    Solve ISBN verifier

--- isbn-verifier/src/main/java/IsbnVerifier.java
+++ isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,17 +1,30 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.regex.Pattern;
 
-class IsbnVerifier {
+public class IsbnVerifier {
 
-  public boolean isValid(String stringToVerify) {
-    if (stringToVerify.length() == 0) return false;
-    String number = stringToVerify.replaceAll("\\D+", "");
-    List<Integer> digits =
-        Arrays.stream(number.split("")).map(d -> Integer.valueOf(d)).collect(Collectors.toList());
-    if (stringToVerify.charAt(stringToVerify.length() - 1) == 'X') digits.add(10);
-    if (digits.size() != 10) return false;
-    return IntStream.range(0, 10).map(i -> (10 - i) * digits.get(i)).sum() % 11 == 0;
-  }
+    public boolean isValid(String isbn) {
+        String cleanedIsbn = isbn.replace("-", "");
+
+        if (cleanedIsbn.length() != 10) {
+            return false;
+        }
+
+        if (!Pattern.matches("[0-9X]{10}", cleanedIsbn)) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += (cleanedIsbn.charAt(i) - '0') * (10 - i);
+        }
+
+        char checkChar = cleanedIsbn.charAt(9);
+        if (checkChar == 'X') {
+            sum += 10;
+        } else {
+            sum += (checkChar - '0');
+        }
+
+        return sum % 11 == 0;
+    }
 }
