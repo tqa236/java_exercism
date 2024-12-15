3398ff287aadbc4d84dd7290402b8e8b15a31148
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2024-12-15 13:25:30 +0100

    Solve nth prime

--- nth-prime/src/main/java/PrimeCalculator.java
+++ nth-prime/src/main/java/PrimeCalculator.java
@@ -1,7 +1,28 @@
 class PrimeCalculator {
 
-    int nth(int nth) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public int nth(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Prime number index must be a positive integer.");
+        }
+
+        int count = 0;
+        int number = 1;
+        while (count < n) {
+            number++;
+            if (isPrime(number)) {
+                count++;
+            }
+        }
+        return number;
     }
 
+    private boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- nth-prime/src/test/java/PrimeCalculatorTest.java
+++ nth-prime/src/test/java/PrimeCalculatorTest.java
@@ -1,8 +1,9 @@
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-import org.junit.Ignore;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 public class PrimeCalculatorTest {
 
@@ -13,30 +14,25 @@ public class PrimeCalculatorTest {
         assertThat(primeCalculator.nth(1)).isEqualTo(2);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testSecondPrime() {
         assertThat(primeCalculator.nth(2)).isEqualTo(3);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testSixthPrime() {
         assertThat(primeCalculator.nth(6)).isEqualTo(13);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testBigPrime() {
         assertThat(primeCalculator.nth(10001)).isEqualTo(104743);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testUndefinedPrime() {
-        assertThrows(
-            IllegalArgumentException.class,
-            () -> primeCalculator.nth(0));
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> primeCalculator.nth(0));
     }
 
 }
