77d4f1afa57a9f77e9c5b705268372538f7adc2d
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2024-12-15 20:50:39 +0100

    Update

--- transpose/src/main/java/Transpose.java
+++ transpose/src/main/java/Transpose.java
@@ -1,10 +1,38 @@
-/*
+import java.util.ArrayList;
+import java.util.List;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+public class Transpose {
+    public String transpose(String toTranspose) {
+        if (toTranspose.isEmpty()) {
+            return "";
+        }
 
-Please remove this comment when submitting your solution.
+        String[] lines = toTranspose.split("\n");
 
-*/
+        for (int i = lines.length - 2; i >= 0; i--) {
+            if (lines[i].length() < lines[i + 1].length()) {
+                lines[i] = String.format("%-" + lines[i + 1].length() + "s", lines[i]);
+            }
+        }
+
+        int maxLength = 0;
+        for (String line : lines) {
+            maxLength = Math.max(maxLength, line.length());
+        }
+
+        List<StringBuilder> transposed = new ArrayList<>();
+        for (int i = 0; i < maxLength; i++) {
+            transposed.add(new StringBuilder());
+        }
+
+        for (int i = 0; i < maxLength; i++) {
+            for (String line : lines) {
+                if (i < line.length()) {
+                    transposed.get(i).append(line.charAt(i));
+                }
+            }
+        }
+
+        return String.join("\n", transposed.stream().map(StringBuilder::toString).toArray(String[]::new));
+    }
+}
