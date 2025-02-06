8d6e1c2680ffe926441b7b5597397b054ad3b9e6
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2025-02-06 09:05:33 +0100

    Update

--- atbash-cipher/src/main/java/Atbash.java
+++ atbash-cipher/src/main/java/Atbash.java
@@ -1,10 +1,38 @@
-/*
+class Atbash {
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    private static final String PLAIN = "abcdefghijklmnopqrstuvwxyz";
+    private static final String CIPHER = "zyxwvutsrqponmlkjihgfedcba";
 
-Please remove this comment when submitting your solution.
+    String encode(String input) {
+        StringBuilder encoded = new StringBuilder();
+        input = input.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int count = 0;
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                encoded.append(CIPHER.charAt(PLAIN.indexOf(c)));
+                count++;
+            } else {
+                encoded.append(c);
+                count++;
+            }
+            if (count == 5) {
+                encoded.append(' ');
+                count = 0;
+            }
+        }
+        return encoded.toString().trim();
+    }
 
-*/
+    String decode(String input) {
+        StringBuilder decoded = new StringBuilder();
+        input = input.replaceAll("[^a-z0-9]", "");
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                decoded.append(PLAIN.charAt(CIPHER.indexOf(c)));
+            } else {
+                decoded.append(c);
+            }
+        }
+        return decoded.toString();
+    }
+}
