0fd621456f0cdb889c95d667986e4e876b41e6b8
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2025-02-06 09:08:43 +0100

    Update

--- house/src/main/java/House.java
+++ house/src/main/java/House.java
@@ -1,10 +1,36 @@
-/*
+class House {
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    private static final String[] VERSES = {
+        "the house that Jack built.",
+        "the malt that lay in the house that Jack built.",
+        "the rat that ate the malt that lay in the house that Jack built.",
+        "the cat that killed the rat that ate the malt that lay in the house that Jack built.",
+        "the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
+        "the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
+        "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
+        "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
+        "the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
+        "the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
+        "the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.",
+        "the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+    };
 
-Please remove this comment when submitting your solution.
+    String verse(int verse) {
+        return "This is " + VERSES[verse - 1];
+    }
 
-*/
+    String verses(int startVerse, int endVerse) {
+        StringBuilder verses = new StringBuilder();
+        for (int i = startVerse; i <= endVerse; i++) {
+            if (i > startVerse) {
+                verses.append("\n");
+            }
+            verses.append(verse(i));
+        }
+        return verses.toString();
+    }
+
+    String sing() {
+        return verses(1, VERSES.length);
+    }
+}
