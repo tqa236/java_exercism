6acfc934f2fd21d928213dbe79c9336364337975
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2025-02-07 08:34:24 +0100

    Update

--- pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,10 +1,68 @@
-/*
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class PythagoreanTriplet {
+    private final int a;
+    private final int b;
+    private final int c;
 
-Please remove this comment when submitting your solution.
+    public PythagoreanTriplet(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
-*/
+    public static TripletListBuilder makeTripletsList() {
+        return new TripletListBuilder();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        PythagoreanTriplet that = (PythagoreanTriplet) obj;
+        return a == that.a && b == that.b && c == that.c;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b, c);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d, %d)", a, b, c);
+    }
+
+    static class TripletListBuilder {
+        private int sum = 0;
+        private int maxFactor = Integer.MAX_VALUE;
+
+        public TripletListBuilder thatSumTo(int sum) {
+            this.sum = sum;
+            return this;
+        }
+
+        public TripletListBuilder withFactorsLessThanOrEqualTo(int maxFactor) {
+            this.maxFactor = maxFactor;
+            return this;
+        }
+
+        public List<PythagoreanTriplet> build() {
+            List<PythagoreanTriplet> triplets = new ArrayList<>();
+            int limit = Math.min(sum / 2, maxFactor);
+            
+            for (int a = 1; a < limit; a++) {
+                for (int b = a + 1; b < sum - a; b++) {  // Allow full range of b
+                    int c = sum - a - b;
+                    if (c > b && c <= maxFactor && a * a + b * b == c * c) {
+                        triplets.add(new PythagoreanTriplet(a, b, c));
+                    }
+                }
+            }
+            return triplets;
+        }
+
+    }
+}
--- roman-numerals/src/main/java/RomanNumerals.java
+++ roman-numerals/src/main/java/RomanNumerals.java
@@ -1,10 +1,24 @@
-/*
+class RomanNumerals {
+    private final int number;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    private static final int[] VALUES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] SYMBOLS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-Please remove this comment when submitting your solution.
+    RomanNumerals(int number) {
+        this.number = number;
+    }
 
-*/
+    String getRomanNumeral() {
+        StringBuilder roman = new StringBuilder();
+        int remainder = number;
+
+        for (int i = 0; i < VALUES.length; i++) {
+            while (remainder >= VALUES[i]) {
+                roman.append(SYMBOLS[i]);
+                remainder -= VALUES[i];
+            }
+        }
+
+        return roman.toString();
+    }
+}
