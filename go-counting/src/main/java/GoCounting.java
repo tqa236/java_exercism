59d16fd834c97f17c5720a15a16706798cb8b583
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2025-02-10 10:21:07 +0100

    Update

--- go-counting/src/main/java/GoCounting.java
+++ go-counting/src/main/java/GoCounting.java
@@ -1,10 +1,113 @@
-/*
+import java.awt.Point;
+import java.util.*;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class GoCounting {
+    private final char[][] board;
+    private final int rows;
+    private final int cols;
 
-Please remove this comment when submitting your solution.
+    public GoCounting(String boardString) {
+        String[] lines = boardString.split("\n");
+        this.rows = lines.length;
+        this.cols = lines[0].length();
+        this.board = new char[rows][cols];
 
-*/
+        for (int i = 0; i < rows; i++) {
+            this.board[i] = lines[i].toCharArray();
+        }
+    }
+
+    public Player getTerritoryOwner(int x, int y) {
+        if (!isValidPoint(x, y)) {
+            throw new IllegalArgumentException("Invalid coordinate");
+        }
+        if (board[y][x] != ' ') {
+            return Player.NONE;
+        }
+        return floodFill(x, y).owner;
+    }
+
+    public Set<Point> getTerritory(int x, int y) {
+        if (!isValidPoint(x, y)) {
+            throw new IllegalArgumentException("Invalid coordinate");
+        }
+        if (board[y][x] != ' ') {
+            return Collections.emptySet();
+        }
+        return floodFill(x, y).territory;
+    }
+
+    public Map<Player, Set<Point>> getTerritories() {
+        Map<Player, Set<Point>> territories = new HashMap<>();
+        territories.put(Player.BLACK, new HashSet<>());
+        territories.put(Player.WHITE, new HashSet<>());
+        territories.put(Player.NONE, new HashSet<>());
+
+        boolean[][] visited = new boolean[rows][cols];
+        
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                if (board[y][x] == ' ' && !visited[y][x]) {
+                    TerritoryInfo info = floodFill(x, y);
+                    territories.get(info.owner).addAll(info.territory);
+                    for (Point p : info.territory) {
+                        visited[p.y][p.x] = true;
+                    }
+                }
+            }
+        }
+        return territories;
+    }
+
+    private boolean isValidPoint(int x, int y) {
+        return x >= 0 && x < cols && y >= 0 && y < rows;
+    }
+
+    private TerritoryInfo floodFill(int x, int y) {
+        Set<Point> territory = new HashSet<>();
+        Set<Character> borderingStones = new HashSet<>();
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(x, y));
+        boolean[][] visited = new boolean[rows][cols];
+        visited[y][x] = true;
+
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+            territory.add(p);
+
+            for (int[] d : new int[][]{{0,1}, {1,0}, {0,-1}, {-1,0}}) {
+                int nx = p.x + d[0];
+                int ny = p.y + d[1];
+
+                if (isValidPoint(nx, ny)) {
+                    char cell = board[ny][nx];
+                    if (cell == ' ' && !visited[ny][nx]) {
+                        queue.add(new Point(nx, ny));
+                        visited[ny][nx] = true;
+                    } else if (cell == 'B' || cell == 'W') {
+                        borderingStones.add(cell);
+                    }
+                }
+            }
+        }
+        
+        Player owner;
+        if (borderingStones.isEmpty() || borderingStones.size() > 1) {
+            owner = Player.NONE;
+        } else {
+            owner = borderingStones.contains('B') ? Player.BLACK : Player.WHITE;
+        }
+        
+        return new TerritoryInfo(owner, territory);
+    }
+
+    private static class TerritoryInfo {
+        final Player owner;
+        final Set<Point> territory;
+
+        TerritoryInfo(Player owner, Set<Point> territory) {
+            this.owner = owner;
+            this.territory = territory;
+        }
+    }
+}
--- go-counting/src/test/java/GoCountingTest.java
+++ go-counting/src/test/java/GoCountingTest.java
@@ -1,22 +1,21 @@
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Ignore;
-import org.junit.Test;
-
-import java.awt.Point;
+import java.awt.*;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class GoCountingTest {
 
     String board5x5 = "  B  \n" +
-                      " B B \n" +
-                      "B W B\n" +
-                      " W W \n" +
-                      "  W  ";
+            " B B \n" +
+            "B W B\n" +
+            " W W \n" +
+            "  W  ";
 
     @Test
     public void blackCorner5x5BoardTest() {
@@ -27,11 +26,10 @@ public class GoCountingTest {
         territory.add(new Point(0, 1));
         territory.add(new Point(1, 0));
 
-        assertEquals(Player.BLACK, gocounting.getTerritoryOwner(0, 1));
-        assertEquals(territory, gocounting.getTerritory(0, 1));
+        assertThat(gocounting.getTerritoryOwner(0, 1)).isEqualTo(Player.BLACK);
+        assertThat(gocounting.getTerritory(0, 1)).isEqualTo(territory);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void whiteCenter5x5BoardTest() {
         GoCounting gocounting = new GoCounting(board5x5);
@@ -39,11 +37,10 @@ public class GoCountingTest {
         Set<Point> territory = new HashSet<>();
         territory.add(new Point(2, 3));
 
-        assertEquals(Player.WHITE, gocounting.getTerritoryOwner(2, 3));
-        assertEquals(territory, gocounting.getTerritory(2, 3));
+        assertThat(gocounting.getTerritoryOwner(2, 3)).isEqualTo(Player.WHITE);
+        assertThat(gocounting.getTerritory(2, 3)).isEqualTo(territory);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void openCorner5x5BoardTest() {
         GoCounting gocounting = new GoCounting(board5x5);
@@ -53,74 +50,56 @@ public class GoCountingTest {
         territory.add(new Point(0, 4));
         territory.add(new Point(1, 4));
 
-        assertEquals(Player.NONE, gocounting.getTerritoryOwner(1, 4));
-        assertEquals(territory, gocounting.getTerritory(1, 4));
+        assertThat(gocounting.getTerritoryOwner(1, 4)).isEqualTo(Player.NONE);
+        assertThat(gocounting.getTerritory(1, 4)).isEqualTo(territory);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void stoneNotTerritory5x5Board() {
         GoCounting gocounting = new GoCounting(board5x5);
 
         Set<Point> territory = new HashSet<>();
 
-        assertEquals(Player.NONE, gocounting.getTerritoryOwner(1, 1));
-        assertEquals(territory, gocounting.getTerritory(1, 1));
+        assertThat(gocounting.getTerritoryOwner(1, 1)).isEqualTo(Player.NONE);
+        assertThat(gocounting.getTerritory(1, 1)).isEqualTo(territory);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void invalidXTooLow5x5Board() {
         GoCounting gocounting = new GoCounting(board5x5);
 
-        IllegalArgumentException expected =
-            assertThrows(
-                IllegalArgumentException.class,
-                () -> gocounting.getTerritory(-1, 1));
-
-        assertThat(expected).hasMessage("Invalid coordinate");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> gocounting.getTerritory(-1, 1))
+                .withMessage("Invalid coordinate");
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void invalidXTooHigh5x5Board() {
         GoCounting gocounting = new GoCounting(board5x5);
 
-        IllegalArgumentException expected =
-            assertThrows(
-                IllegalArgumentException.class,
-                () -> gocounting.getTerritory(5, 1));
-
-        assertThat(expected).hasMessage("Invalid coordinate");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> gocounting.getTerritory(5, 1))
+                .withMessage("Invalid coordinate");
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void invalidYTooLow5x5Board() {
         GoCounting gocounting = new GoCounting(board5x5);
 
-        IllegalArgumentException expected =
-            assertThrows(
-                IllegalArgumentException.class,
-                () -> gocounting.getTerritory(1, -1));
-
-        assertThat(expected).hasMessage("Invalid coordinate");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> gocounting.getTerritory(1, -1))
+                .withMessage("Invalid coordinate");
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void invalidYTooHigh5x5Board() {
         GoCounting gocounting = new GoCounting(board5x5);
 
-        IllegalArgumentException expected =
-            assertThrows(
-                IllegalArgumentException.class,
-                () -> gocounting.getTerritory(1, 5));
-
-        assertThat(expected).hasMessage("Invalid coordinate");
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> gocounting.getTerritory(1, 5))
+                .withMessage("Invalid coordinate");
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void oneTerritoryIsWholeBoardTest() {
         GoCounting gocounting = new GoCounting(" ");
@@ -135,10 +114,9 @@ public class GoCountingTest {
         territories.put(Player.WHITE, whiteTerritory);
         territories.put(Player.NONE, noneTerritory);
 
-        assertEquals(territories, gocounting.getTerritories());
+        assertThat(gocounting.getTerritories()).isEqualTo(territories);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void twoTerritoryRectangularBoardTest() {
         GoCounting gocounting = new GoCounting(" BW \n BW ");
@@ -158,10 +136,9 @@ public class GoCountingTest {
         territories.put(Player.WHITE, whiteTerritory);
         territories.put(Player.NONE, noneTerritory);
 
-        assertEquals(territories, gocounting.getTerritories());
+        assertThat(gocounting.getTerritories()).isEqualTo(territories);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void twoRegionRectangularBoardTest() {
         GoCounting gocounting = new GoCounting(" B ");
@@ -177,6 +154,6 @@ public class GoCountingTest {
         territories.put(Player.WHITE, whiteTerritory);
         territories.put(Player.NONE, noneTerritory);
 
-        assertEquals(territories, gocounting.getTerritories());
+        assertThat(gocounting.getTerritories()).isEqualTo(territories);
     }
 }
